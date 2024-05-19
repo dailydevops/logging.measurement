cab51b2d3d0dd80e44a01fb42f255e66706c96ad
Martin Stühmer <me@samtrion.net>
2024-05-19 10:33:37 +0200

    chore: Added Package Reference to `NetEvolve.Logging.XUnit`

--- tests/NetEvolve.Logging.Measurement.Tests.Integration/MeasurementScopeExtensionsTests.cs
+++ tests/NetEvolve.Logging.Measurement.Tests.Integration/MeasurementScopeExtensionsTests.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using NetEvolve.Logging.Abstractions;
 using NetEvolve.Logging.XUnit;
 using Xunit;
 using Xunit.Abstractions;
